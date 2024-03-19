eea2fae5b701f7e3d9b0132829fcd126594b2e3a
Nate Butler <iamnbutler@gmail.com>
2024-03-19 17:17:45 -0400

    Update OpenAI service to accept dynamic user content

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import * as fileService from "./services/fileService";
 import * as openaiService from "./services/openaiService";
 
 dotenv.config();
@@ -8,16 +7,17 @@ dotenv.config();
 const app = express();
 const port: number = parseInt(process.env.PORT || "3000");
 
+app.use(express.json());
+
 app.post("/openai/completion", async (req: Request, res: Response) => {
   try {
-    const completionResult = await openaiService.completion();
+    const content: string = req.body.content;
+    const completionResult = await openaiService.completion(content);
     res.json({ completion: completionResult });
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching the completion." });
   }
 });
-
-app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/server/services/openaiService.ts
+++ src/server/services/openaiService.ts
@@ -15,17 +15,21 @@ const openai = new OpenAI({ apiKey: openai_key });
 
 const MODEL = "gpt-4-0125-preview";
 
-export async function completion(): Promise<string> {
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "You are a helpful assistant designed to output JSON." },
-      { role: "user", content: "Who won the world series in 2020?" },
-      { role: "assistant", content: "The Los Angeles Dodgers won the World Series in 2020." },
-      { role: "user", content: "Where was it played?" },
-    ],
-    model: MODEL,
-    response_format: { type: "json_object" },
-  });
+export async function completion(content: string): Promise<string> {
+  try {
+    const completionResult = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant designed to output JSON." },
+        { role: "user", content },
+      ],
+      model: MODEL,
+      response_format: { type: "json_object" },
+    });
 
-  return completion.choices[0].message.content || "No response";
+    const messageContent = completionResult.choices[0].message.content;
+
+    return messageContent || "No response";
+  } catch (error) {
+    throw error;
+  }
 }
