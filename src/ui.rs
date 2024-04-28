ff7f36c54165c67b844d7ac8c3fb02086a993f97
Nate Butler <iamnbutler@gmail.com>
2024-04-28 00:08:41 -0400

    add model-driven Game

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,22 @@
 use gpui::*;
 
+use crate::Game;
+
 #[derive(Debug)]
-pub struct GameWindow {}
+pub struct GameWindow {
+    pub game: Game,
+}
+
+impl GameWindow {
+    pub fn new(game: Game) -> Self {
+        Self { game }
+    }
+}
 
 impl Render for GameWindow {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let player = self.game.data.read(cx).player.clone();
+
         div()
             .flex()
             .bg(hsla(0.0, 0.0, 0.0, 1.0))
@@ -13,6 +25,6 @@ impl Render for GameWindow {
             .items_center()
             .text_xl()
             .text_color(hsla(1.0, 1.0, 1.0, 1.0))
-            .child(format!("hello world"))
+            .child(format!("Hello, {}!", player.name))
     }
 }
